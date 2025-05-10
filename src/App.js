e27b9ae175eff46dc54fbc3d3d500cdf6418a4ca
minho-baek <chaplin8977@gmail.com>
2025-05-10 19:07:45 -0400

    React useState

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
-// import logo from './logo.svg';
-// import './App.css';
+import { useState } from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header> 
-//     </div>
-//   );
-// }
-
-// export default App;
 function App() {
-  // const number = 1;
-
-  // const double = () => {
-  //   return number * 2
-  // }
-  // double();
-
-  const printHello = () => {
-    console.log('hello')
+  const [number, setNumber] = useState(1);
+  const double = () => {
+    setNumber(number * 2);
+    console.log(number);
   }
 
   return (
     <>
-      {/* <div>{double(number)}</div> */}
-      <button onClick={printHello}>Submit</button>
+      <div>{number}</div>
+      <button onClick={double}>Submit</button>
     </>
   )
 }
